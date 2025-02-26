c8a131c7f1551675e019788238d7577daf57a8d2
dehydrated bear <harshit01820@gmail.com>
2025-02-26 19:52:05 +0530

    agent

--- agents/urls.py
+++ agents/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import AgentListView,AgentCreateView
+from .views import AgentListView,AgentCreateView,AgentDetailView,AgentUpdateView,AgentDeleteView
 
 app_name='agent'
 
 urlpatterns=[
 
     path('',AgentListView.as_view(),name="agent_list"),
-    path('create/',AgentCreateView.as_view(),name="agent_create")
-
+    path('create/',AgentCreateView.as_view(),name="agent_create"),
+    path('<int:pk>/',AgentDetailView.as_view(),name="agent_view"),
+    path('<int:pk>/update/',AgentUpdateView.as_view(),name="agent_update"),
+    path('<int:pk>/delete/',AgentDeleteView.as_view(),name="agent_delete")
 ]
