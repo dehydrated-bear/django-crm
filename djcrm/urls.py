dae9c0106d51adee996ea6a6255381dc3494cd2f
harshit <harshit01820@gmail.com>
2025-02-27 14:12:39 +0530

    password reset

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -19,7 +19,13 @@ from django.urls import path,include
 from leads.views import landing_page ,landingpageview,SignupView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView)
 
 
 urlpatterns = [
@@ -27,6 +33,10 @@ urlpatterns = [
     path('leads/',include('leads.urls',namespace="leads")),
     path('agents/',include('agents.urls',namespace="agents")),
     path('',landingpageview.as_view(),name="landing_page"),
+    path('reset-password/',PasswordResetView.as_view(),name="reset-password"),
+    path('password-reset-done/',PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name="password-reset-confirm"),
     path('login/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
     path('signup/' , SignupView.as_view(),name="signup")
